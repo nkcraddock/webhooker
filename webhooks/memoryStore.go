392b3f9899ddc8661c3462335a6e6b7c50bc623a
Nathan Craddock <nkcraddock@gmail.com>
2015-02-20 23:41:59 -0500

    Removed some debugging printlns

--- webhooks/memoryStore.go
+++ webhooks/memoryStore.go
@@ -1,7 +1,5 @@
 package webhooks
 
-import "fmt"
-
 type memoryStore struct {
 	hookers map[string]Webhooker
 	hooks   map[string]Webhook
@@ -15,8 +13,6 @@ func newMemoryStore() *memoryStore {
 }
 
 func (ms *memoryStore) getAll(hooker string) (hooks []Webhook) {
-	fmt.Println(">>>> HOOKERS >>>>> ", ms.hookers)
-	fmt.Println(">>>> HOOKS >>>>> ", ms.hooks)
 	hooks = make([]Webhook, 0)
 	for _, h := range ms.hooks {
 		hooks = append(hooks, h)
